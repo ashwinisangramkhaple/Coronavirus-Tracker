628e8546c08fcad3859f191e1a116f0da4b3a527
Ashwini <ashwinikhaple28@gmail.com>
2022-05-28 21:06:16 +0530

    created a service to fetch data from the given url

--- coronavirus-tracker/src/main/java/com/ask/coronavirustracker/CoronavirusTrackerApplication.java
+++ coronavirus-tracker/src/main/java/com/ask/coronavirustracker/CoronavirusTrackerApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoronavirusTrackerApplication {
+public class CoronavirusTrackerApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(CoronavirusTrackerApplication.class, args);
 	}
 
